1078e0f4b8dcbc4f9f79e4be675ee48cf0b5077d
Sourin Roy <144391871+sourinry@users.noreply.github.com>
2025-07-24 10:56:40 +0530

    find user function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,27 @@
+const userModel = require("../models/userModel");
+
 const getUserController = async (req,res) => {
     try {
+        console.log(req.user.id); // check whats in the payload
+        /*const { id } = req.user;
+        if(!id){
+            return res.status(401).send({
+                success: false,
+                message: "id is not fund in payload"
+            });
+        }*/
+        const user = await userModel.findById({_id:req.user.id});
+        if(!user){
+            return res.send(404).send({
+                success: false,
+                message: "user not found"
+            });
+        }
         res.status(200).send({
             success: true,
-            message: "authincation done"
-        })
-
+            message: "authincation done",
+            user
+        });
     } catch (error) {
         console.log(error);
         res.status(505).send({
